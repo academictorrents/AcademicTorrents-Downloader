4649d3b5c35ac7a4831e3084fdc58d6328832566
Joseph Paul Cohen <joecohen@ieee.org>
2014-06-14 17:13:47 -0400

    more work

--- src/Main.java
+++ src/Main.java
@@ -6,9 +6,11 @@ import java.io.IOException;
 import java.net.URL;
 import java.security.NoSuchAlgorithmException;
 
+import org.apache.commons.math3.stat.descriptive.DescriptiveStatistics;
 import org.bitlet.wetorrent.Metafile;
 import org.bitlet.wetorrent.Torrent;
 import org.bitlet.wetorrent.disk.PlainFileSystemTorrentDisk;
+import org.bitlet.wetorrent.disk.ResumeListener;
 import org.bitlet.wetorrent.disk.TorrentDisk;
 import org.bitlet.wetorrent.peer.IncomingPeerListener;
 
@@ -25,18 +27,74 @@ public class Main {
         String filename = args[0];
 
         // Parse the metafile
-        Metafile metafile = new Metafile(new BufferedInputStream(new FileInputStream(new File(filename))));
-
+        //final Metafile metafile = new Metafile(new BufferedInputStream(new FileInputStream(new File(filename))));
+        
+        final Metafile metafile;
+        if (new File(filename).exists()){
+        	metafile = new Metafile(new BufferedInputStream(new FileInputStream(new File(filename))));
+        }else if (filename.startsWith("http")){
+        	metafile = new Metafile(new BufferedInputStream(new URL(filename).openStream()));
+        }else{
+        	metafile = new Metafile(new BufferedInputStream(new URL("http://academictorrents.com/download/" + filename).openStream()));
+        }
+        
+        if (args.length > 1){
+        	
+        	if (args[1].equals("ls")){
+        		
+        		System.out.println("File Listing:");
+        		
+        		for (Object s : metafile.getFiles())
+                	System.out.println(s);
+        		
+        	}
+        	
+        	
+        	//System.exit(0);
+        }
+        
+        
+//        for (Object s : metafile.getInfo().keySet())
+//        	System.out.println(s + " " + metafile.getInfo().get(s));
+//        
+        
         // Create the torrent disk, this is the destination where the torrent file/s will be saved
         TorrentDisk tdisk = new PlainFileSystemTorrentDisk(metafile, new File("."));
-        tdisk.init();
+        
+        if (tdisk.init()){
+	        tdisk.resume(new ResumeListener() {
+				
+				@Override
+				public void percent(long completed, long resumed) {
+					
+					for (int i = 0; i< 80; i++){
+		            	System.out.print("\b");
+		            }
+					
+					int done = (int) ((completed*1.0/metafile.getLength()*1.0)*100);
+		            
+		            System.out.print("Resuming (have/scanned/total) " + 
+		            humanReadableByteCount(resumed,true) + "/" + 
+		            humanReadableByteCount(completed,true) + "/" + 
+		            humanReadableByteCount(metafile.getLength(),true) + ", " + 
+		            done + "%");
+					
+				}
+			});
+        }
         
         IncomingPeerListener peerListener = new IncomingPeerListener(PORT);
         peerListener.start();
 
         Torrent torrent = new Torrent(metafile, tdisk, peerListener);
+        
+        
+        System.out.println();
         torrent.startDownload();
 
+        DescriptiveStatistics stats = new DescriptiveStatistics();
+        Long previoussize = torrent.getTorrentDisk().getCompleted();
+        int previousStringLength = 100;
         while (!torrent.isCompleted()) {
 
             try {
@@ -46,18 +104,44 @@ public class Main {
             }
 
             torrent.tick();
-            System.out.printf("Got %s peers, completed %d bytes\n",
-                    torrent.getPeersManager().getActivePeersNumber(),
-                    torrent.getTorrentDisk().getCompleted());
+            for (int i = 0; i< previousStringLength+1; i++){
+            	System.out.print("\b");
+            }
+            
+            
+            int done = (int) ((torrent.getTorrentDisk().getCompleted()*1.0/metafile.getLength()*1.0)*100);
+            
+            stats.addValue(torrent.getTorrentDisk().getCompleted() - previoussize);
+            previoussize = torrent.getTorrentDisk().getCompleted();
+            
+            String toprint = String.format("Downloading (have/total) %s/%s, " + done + "%%, %s bytes/sec, %s peers",
+            		humanReadableByteCount(torrent.getTorrentDisk().getCompleted(), true),
+            		humanReadableByteCount(metafile.getLength(), true),
+            		humanReadableByteCount((long) stats.getMean(), true),
+            		torrent.getPeersManager().getActivePeersNumber()
+            		);
+            previousStringLength = toprint.length();
+            System.out.print(toprint);
+            
         }
         
-        System.out.println("Finished");
-        torrent.interrupt();
-        peerListener.interrupt();
+        System.out.println("\nFinished");
+        System.exit(0);
+        //torrent.interrupt();
+        //peerListener.interrupt();
         
 		
 		
 		
+	}
+	
+	
+	public static String humanReadableByteCount(long bytes, boolean si) {
+	    int unit = si ? 1000 : 1024;
+	    if (bytes < unit) return bytes + " B";
+	    int exp = (int) (Math.log(bytes) / Math.log(unit));
+	    String pre = (si ? "kMGTPE" : "KMGTPE").charAt(exp-1) + (si ? "" : "i");
+	    return String.format("%.1f %sB", bytes / Math.pow(unit, exp), pre);
 	}
 
 }
