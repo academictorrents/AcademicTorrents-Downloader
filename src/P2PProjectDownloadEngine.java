a12e9037793b8dd2b560e8a9bb936f22944d863d
Joseph Paul Cohen <joecohen@ieee.org>
2014-06-20 15:36:42 -0400

    partial

---
+++ src/P2PProjectDownloadEngine.java
@@ -0,0 +1,46 @@
+
+
+public class P2PProjectDownloadEngine {
+	
+	private static final int PORT = 6881;
+
+	public static void main(String[] args) throws Exception {
+		System.out.println("Welcome to the Academic Torrents Download tool!");
+
+		
+        // read torrent filename from command line arg
+        String filename = args[0];
+
+   
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        System.out.println("\nFinished");
+        System.exit(0);
+        //torrent.interrupt();
+        //peerListener.interrupt();
+        
+		
+		
+		
+	}
+	
+	
+	public static String humanReadableByteCount(long bytes, boolean si) {
+	    int unit = si ? 1000 : 1024;
+	    if (bytes < unit) return bytes + " B";
+	    int exp = (int) (Math.log(bytes) / Math.log(unit));
+	    String pre = (si ? "kMGTPE" : "KMGTPE").charAt(exp-1) + (si ? "" : "i");
+	    return String.format("%.1f %sB", bytes / Math.pow(unit, exp), pre);
+	}
+
+}
---
+++ src/VuzeATDownloadEngine.java
@@ -0,0 +1,121 @@
+import java.io.File;
+import java.util.List;
+
+import org.gudy.azureus2.core3.disk.DiskManagerFileInfo;
+import org.gudy.azureus2.core3.download.DownloadManager;
+import org.gudy.azureus2.core3.download.DownloadManagerListener;
+import org.gudy.azureus2.core3.global.GlobalManager;
+
+import com.aelitis.azureus.core.AzureusCore;
+import com.aelitis.azureus.core.AzureusCoreException;
+import com.aelitis.azureus.core.AzureusCoreFactory;
+
+
+public class VuzeATDownloadEngine {
+
+	public static void download(File downloadedTorrentFile) throws InterruptedException {
+		
+    
+	    new File(Main.ATDIR + "/az-config").delete();
+	    System.setProperty("azureus.config.path", Main.ATDIR + "/az-config");
+	    
+	    AzureusCore core = AzureusCoreFactory.create();
+	    core.start();
+	    
+	    
+	    //System.out.println("Completed download of : " + downloadedTorrentFile.getName());
+	    //System.out.println("File stored as : " + downloadedTorrentFile.getAbsolutePath());
+	    
+	    File downloadDirectory = new File("."); //Destination directory
+	    //if(downloadDirectory.exists() == false) downloadDirectory.mkdir();
+	    
+	    //Start the download of the torrent 
+	    GlobalManager globalManager = core.getGlobalManager();
+	    DownloadManager manager = globalManager.addDownloadManager(downloadedTorrentFile.getAbsolutePath(),
+	                                                               downloadDirectory.getAbsolutePath());
+	    System.out.println("Downloading");
+	    DownloadManagerListener listener = new DownloadStateListener();
+	    manager.addListener(listener);    
+	    globalManager.startAllDownloads();
+	    //core.requestStop();
+
+	}
+
+}
+
+
+class DownloadStateListener implements DownloadManagerListener {
+
+	public void stateChanged(DownloadManager manager, int state) {
+		switch (state) {
+		case DownloadManager.STATE_DOWNLOADING:
+			System.out.println("Downloading....");
+			// Start a new daemon thread periodically check
+			// the progress of the upload and print it out
+			// to the command line
+			Runnable checkAndPrintProgress = new Runnable() {
+
+				public void run() {
+					try {
+						boolean downloadCompleted = false;
+						while (!downloadCompleted) {
+							AzureusCore core = AzureusCoreFactory
+									.getSingleton();
+							List<DownloadManager> managers = core
+									.getGlobalManager().getDownloadManagers();
+
+							// There is only one in the queue.
+							DownloadManager man = managers.get(0);
+							System.out.println("Download is "
+									+ (man.getStats().getCompleted() / 10.0)
+									+ " % complete");
+							downloadCompleted = man.isDownloadComplete(true);
+							// Check every 10 seconds on the progress
+							Thread.sleep(10000);
+						}
+					} catch (Exception e) {
+						throw new RuntimeException(e);
+					}
+
+				}
+			};
+
+			Thread progressChecker = new Thread(checkAndPrintProgress);
+			progressChecker.setDaemon(true);
+			progressChecker.start();
+			break;
+		}
+	}
+
+	public void downloadComplete(DownloadManager manager) {
+		System.out.println("Download Completed - Exiting.....");
+		AzureusCore core = AzureusCoreFactory.getSingleton();
+		try {
+			core.requestStop();
+		} catch (AzureusCoreException aze) {
+			System.out.println("Could not end Azureus session gracefully - "
+					+ "forcing exit.....");
+			core.stop();
+		}
+	}
+
+	@Override
+	public void completionChanged(DownloadManager manager, boolean bCompleted) {
+		System.out.println("completionChanged");
+		
+	}
+
+	@Override
+	public void positionChanged(DownloadManager download, int oldPosition,
+			int newPosition) {
+		System.out.println("positionChanged");
+		
+	}
+
+	@Override
+	public void filePriorityChanged(DownloadManager download,
+			DiskManagerFileInfo file) {
+		System.out.println("filePriorityChanged");
+		
+	}
+}
