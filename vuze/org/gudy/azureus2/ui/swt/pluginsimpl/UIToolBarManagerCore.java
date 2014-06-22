3640ec03a7d65e4dadd966e5ad78606f0042af38
Joseph Paul Cohen <joecohen@ieee.org>
2014-06-22 02:21:25 -0400

    got rid of swt

--- vuze/org/gudy/azureus2/ui/swt/pluginsimpl/UIToolBarManagerCore.java
+++
@@ -1,9 +0,0 @@
-package org.gudy.azureus2.ui.swt.pluginsimpl;
-
-
-import org.gudy.azureus2.plugins.ui.toolbar.UIToolBarManager;
-
-public interface UIToolBarManagerCore
-	extends UIToolBarManager
-{
-}
